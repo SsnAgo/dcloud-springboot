2a6bd7d5067f88909d6e733dad41e74272b6e5b0
SsnAgo <954752598@qq.com>
2021-03-27 21:48:31 +0800

    2

--- src/main/java/com/example/dcloud/DcloudApplication.java
+++ src/main/java/com/example/dcloud/DcloudApplication.java
@@ -1,9 +1,11 @@
 package com.example.dcloud;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.example.dcloud.mapper")
 public class DcloudApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/example/dcloud/config/Swagger2Config.java
+++ src/main/java/com/example/dcloud/config/Swagger2Config.java
@@ -24,6 +24,7 @@ public class Swagger2Config {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
                 .select()
+                // 访问某个包下的requestHandler
                 .apis(RequestHandlerSelectors.basePackage("com.example.dcloud.controller"))
                 // 可以访问任何路径
                 .paths(PathSelectors.any())
@@ -43,6 +44,7 @@ public class Swagger2Config {
 
     private List<SecurityContext> securityContexts(){
         List<SecurityContext> res = new ArrayList<>();
+        // 设置不需要认证就可以访问的接口
         res.add(getContextByPath("/hello/.*"));
         return res;
     }
@@ -61,6 +63,13 @@ public class Swagger2Config {
     }
 
     // ApiKey继承于SecurityScheme抽象类
+    /**
+     * 设置完成后进入SwaggerUI，右上角出现“Authorization”按钮，点击即可输入我们配置的认证参数。
+     * 对于不需要输入参数的接口（上文所述的包含hello的接口），在未输入Authorization参数就可以访问。
+     * 其他接口则将返回401错误。点击右上角“Authorization”按钮，输入配置的参数后即可通过认证访问。参数输入后全局有效，无需每个接口单独输入。
+     * 通过Swagger2的securitySchemes配置全局参数：如下列代码所示，securitySchemes的ApiKey中增加一个名为“Authorization”，type为“header”的参数。
+     * @return
+     */
     private List<ApiKey> securitySchemes(){
         List<ApiKey> res = new ArrayList<>();
         ApiKey apiKey = new ApiKey("authorization","authorization","Header");
